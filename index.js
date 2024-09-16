05278d8e89d0c00c8865474f13da51e65f197985
granjerolucas <56519538+granjerolucas@users.noreply.github.com>
2024-09-16 10:04:53 -0500

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const btnSubmit = document.querySelector(".btn-subscribe");
+const lblError = document.querySelector(".text-error");
+const emailInput = document.querySelector("#emailInput");
+const emailConfim = document.querySelector("#emailConfim");
+const modal = new bootstrap.Modal('#exampleModal')
+const errorMessage = "Valid email required";
+modal.show()
+
+btnSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+  const email = emailInput.value;
+
+  // varlidar email
+  let success = true;
+  if (email.length > 4) {
+    const indexOfAt = email.indexOf("@");
+    const indexOfPoint = email.indexOf(".", indexOfAt);
+    if (indexOfPoint > indexOfAt && indexOfPoint + 1 < email.length) {
+      // console.log(email);
+      emailConfim.textContent = email;
+      modal.show()
+    } else {
+      success = false;
+    }
+  } else {
+    success = false;
+  }
+  if (!success) showError(errorMessage);
+});
+
+function showError(message) {
+  console.log(message);
+  lblError.textContent = message;
+  lblError.classList.remove("d-none");
+  setTimeout(() => {
+    lblError.classList.add("d-none");
+  }, 5000);
+}
+
+
+function onClickClose () {
+  emailInput.value = ""
+  modal.hide()
+}
